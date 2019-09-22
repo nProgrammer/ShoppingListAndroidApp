246a204a5e625c07c925e28d69d929961aefce56
nProgrammer <wagner.norbert277@gmail.com>
2019-09-22 22:11:21 +0200

    Added tests

---
+++ app/src/test/java/pl/norbertwagner/nprogrammer/shoppinglist/MathIsCorrectTest.kt
@@ -0,0 +1,11 @@
+package pl.norbertwagner.nprogrammer.shoppinglist
+
+import org.junit.Assert
+import org.junit.Test
+
+class MathIsCorrectTest {
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(0, 1000 % 2)
+    }
+}
