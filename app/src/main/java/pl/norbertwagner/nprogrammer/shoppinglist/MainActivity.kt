e728dbb43ab1d21e88f1f51986566459388ed617
nProgrammer <wagner.norbert277@gmail.com>
2019-09-23 19:52:41 +0200

    Added Icons

--- app/src/main/java/pl/norbertwagner/nprogrammer/shoppinglist/MainActivity.kt
+++ app/src/main/java/pl/norbertwagner/nprogrammer/shoppinglist/MainActivity.kt
@@ -1,22 +1,21 @@
 package pl.norbertwagner.nprogrammer.shoppinglist
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.GridLayout
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.item_view.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("ShowToast")
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,18 +31,18 @@ class MainActivity : AppCompatActivity() {
             val product = itemUserText.text.toString()
             val dateTime = LocalDateTime.now()
             val date = dateTime.format(DateTimeFormatter.ofPattern("M/d/y"))
-            if(!product.isNullOrEmpty()) {
+            if(product.isNotEmpty()) {
                 val value = ContentValues()
                 value.put("product", product)
                 value.put("date", date)
                 db.insertOrThrow(TableInfo.TABLE_NAME, null, value)
                 refresh(db)
             }else{
-                Toast.makeText(applicationContext, "Nie podałeś produktu", Toast.LENGTH_SHORT)
+                Toast.makeText(applicationContext, "Nie podałeś produktu", Toast.LENGTH_SHORT).show()
             }
         }
     }
-    fun refresh(db: SQLiteDatabase){
+    private fun refresh(db: SQLiteDatabase){
         recycler_view.layoutManager = GridLayoutManager(applicationContext, 2)
         recycler_view.adapter = ItemViewAdapter(applicationContext, db)
     }
